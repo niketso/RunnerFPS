0c60b0c5272c994fdb8973894aba7c1a5ed9ca08
Unknown <nicolaspiccitto@gmail.com>
2018-08-21 17:32:59 -0300

    TPMoviles-v0.1
    Prueba de movimiento del personaje

---
+++ TPMoviles/Assets/Scripts/PlayerMov.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov : MonoBehaviour {
+    [SerializeField] private float speed;
+    CharacterController cc;
+    private void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+    private void Update()
+    {
+        PMov();
+    }
+
+    private void PMov()
+    {
+        float HorMov = Input.GetAxis("Horizontal") * speed;
+        Vector3 vec = new Vector3(HorMov * Time.deltaTime, 0 , speed * Time.deltaTime);
+        cc.Move(vec);
+        
+    }
+}
