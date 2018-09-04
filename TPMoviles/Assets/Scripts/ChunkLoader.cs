08b0a631d6c4ef97212b131c6a518414a29a0566
Lisandro <li_gueva@hotmail.com>
2018-09-04 15:19:21 -0300

    TPMoviles-v0.3
    chunk loader script

---
+++ TPMoviles/Assets/Scripts/ChunkLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkLoader : MonoBehaviour {
+
+    public GameObject[] items;
+
+	// Use this for initialization
+	void Start () {
+
+        var temp = Resources.LoadAll("Chunks/", typeof(GameObject));
+
+        items = new GameObject[temp.GetLength(0)];
+
+        Debug.Log(items);
+        int i = 0;
+
+        foreach (GameObject chunk in temp)
+        {
+            items[i] = chunk;
+            i++;
+            Debug.Log("un chunk cargado");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ TPMoviles/Assets/Scripts/LevelCreator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCreator : MonoBehaviour {
+
+
+
+	void Start ()
+    {
+		
+	}
+
+    private void SpawnChunk()
+    {
+
+    }
+}
